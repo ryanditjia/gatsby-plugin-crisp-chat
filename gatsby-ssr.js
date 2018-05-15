9d17f35600710d9c283ee4cd9467d207d3c3a99c
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-05-15 13:21:13 +0700

    Added babel build step

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,15 @@
-import React from 'react'
+"use strict";
 
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  const crispConfig = `window.$crisp=[];window.CRISP_WEBSITE_ID="${pluginOptions.websiteId}";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
+var _react = require("react");
 
-  return setHeadComponents([
-    <script key="gatsby-plugin-crisp-chat" dangerouslySetInnerHTML={{ __html: crispConfig }} />,
-  ])
-}
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.onRenderBody = function (_ref, pluginOptions) {
+  var setHeadComponents = _ref.setHeadComponents;
+
+  var crispConfig = "window.$crisp=[];window.CRISP_WEBSITE_ID=\"" + pluginOptions.websiteId + "\";(function(){d=document;s=d.createElement(\"script\");s.src=\"https://client.crisp.chat/l.js\";s.async=1;d.getElementsByTagName(\"head\")[0].appendChild(s);})();";
+
+  return setHeadComponents([_react2.default.createElement("script", { key: "gatsby-plugin-crisp-chat", dangerouslySetInnerHTML: { __html: crispConfig } })]);
+};
